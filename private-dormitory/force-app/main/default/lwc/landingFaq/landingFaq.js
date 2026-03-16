7f9243a9ab849eb793cbb54e9a3c4587e4ab6cef
mariiaanna <cherkunmariia@gmail.com>
2026-03-17 01:42:17 +0200

    Revert "Home UI"

--- private-dormitory/force-app/main/default/lwc/landingFaq/landingFaq.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class LandingFaq extends LightningElement {
-    faqs = [
-        { q: 'Are pets allowed?', a: 'Unfortunately, pets are not allowed according to our rules.' },
-        { q: 'What documents are required?', a: 'Passport, tax ID, and university enrollment certificate.' },
-        { q: 'Is there a curfew?', a: 'No, residents have 24/7 access with electronic key cards.' }
-    ];
-}
--- private-dormitory/force-app/main/default/lwc/landingFooter/landingFooter.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class LandingFooter extends LightningElement {
-    currentYear = new Date().getFullYear();
-}
